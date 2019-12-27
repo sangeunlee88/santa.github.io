4e0036d73b19925a5dcfd14b4c533c8060e29007
sangeunlee88 <sangeun.lee.88@gmail.com>
2019-12-27 20:25:57 +0900

    added from osx

---
+++ _posts/2019-12-27-workspace-for-os-x.md
@@ -0,0 +1,4 @@
+# OS X 관련 단축키 정리
+
+Command + Shift + g: 경로 입력창 
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Workspace For Os X",
+        "excerpt":"OS X 관련 단축키 정리   Command + Shift + g: 경로 입력창   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/workspace-for-os-x/",
+        "teaser":null}]
