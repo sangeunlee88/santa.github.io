6cd83867afb02ed508de89f9e02b992c6c95d101
sangeunlee88 <sangeun.lee.88@gmail.com>
2020-01-06 10:54:20 +0900

    1_6

--- _posts/2019-12-27-workspace-for-os-x.md
+++ _posts/2019-12-27-workspace-for-os-x.md
@@ -14,6 +14,12 @@ data: "2019-12-27 23:00"
 code ~/.zprofile 
 alias desk='cd ~/Desktop'
 alias cl='clear' 
+
+df -h  현재남은 용량 확인
+
+la 
+las
+ 
 ```
 
 ### Network 
@@ -52,13 +58,75 @@ conda activate [name] ### activate env.
 ```
 ## OS X 관련 단축키 정리
 
-Command + Shift + g : 경로 입력창 
+Command + Shift + g : 경로 입력창   
 Command + Shift + h : 이전에 작성한 명령어 확인 (터미널)
 
 ## github.io 홈페이지 관련 
 
 ```
 bundle exec jekyll serve ### port 4000 으로 확인 가능 
+```
+
+
+
+
+## pip 
+
+```
+pip install [name] --upgrade ### upgrade package 
+
+pip install freeze 
+pip freeze > requirements.txt : Save installed packages
+pip install -r requirements.txt
+
+pip install --upgrade pip
+
+```
+
+
+
+
+## Docker 
+
+```
+docker commit [container name] [new image name] ### docker image 작성업 
+
+```
+
+## Vim
+
+```
+w <-> b 단어단위
+e : 단어의 끝 마지막
+hjkl : 방향키
+^ 첫번째 문자
+0
+
+$ 문장끝
+
+문자 찾기
+
+f+:문자 , ;
+t+문자, ;(계속검색,정방향) , (역방향) 
+/(문자 검색) 소문자 n
+x: 삭제
+dd: 한문장 삭제  
+D 현재문장에서 끝까지
+J 위로 당겨주기
+p P 붙여넣기
+dj
+dj
+
+y 한줄 복사
+u 작업취소
+ctr r
+
+```
+
+
+PyCharm 리모트 
+```
+ssh -f (name) -L 5000(Localpy):localhost:22 -N
 ```
 
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,21 @@
 var store = [{
         "title": "Workspace For Os X",
-        "excerpt":"Linux Command ### alias을 이용한 단축기 설정법 code ~/.zprofile alias desk='cd ~/Desktop' alias cl='clear' Network lsof -i:[Port No.] : 해당하는 Port와 관련된 프로세스 확인 kill -9 [해당하는 PID No.] : o: -9 강제종료 ssh -f lee -L [target port]:localhost:[local port] -N Github git init git add . git commit -m...","categories": [],
+        "excerpt":"Linux Command ### alias을 이용한 단축기 설정법 code ~/.zprofile alias desk='cd ~/Desktop' alias cl='clear' df -h 현재남은 용량 확인 la las Network lsof -i:[Port No.] : 해당하는 Port와 관련된 프로세스 확인 kill -9 [해당하는 PID No.] : o: -9 강제종료 ssh -f lee -L [target port]:localhost:[local port] -N Github git...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/workspace-for-os-x/",
+        "teaser":null},{
+        "title": "Python",
+        "excerpt":"Scope of variable in class   0variable : public  1__variable : private      public     class Test():            prevate   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/python/",
+        "teaser":null},{
+        "title": "Torch",
+        "excerpt":"torch.Tensor 작성과 기본동작 torch.cat() device = torch.device(\"cuda:0\") x1 = torch.Tensor([[1, 1], [1, 1], [1, 1]]).to(self.device) x2 = torch.Tensor([[2, 2], [2, 2], [2, 2]]).to(self.device) x3 = torch.cat([x1, x2], dim = 0) x4 = torch.cat([x1, x2], dim = 1) print(x3.is_cuda) tensor([[1., 1.], [1., 1.], [1., 1.]], device='cuda:0') tensor([[2., 2.], [2., 2.], [2.,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/torch/",
+        "teaser":null},{
+        "title": "Papers 0",
+        "excerpt":"SiCloPe: Silhouette-Based Clothed People 요약 3차원으로 사람을 표현하는 방법에 대한 복잡성을 줄이기 위해서 SCAPE과 SMPL같은 pose와 Shape의 파라메터를 이용하안 3차원 인간 모델이 소개 되었습니다. 그러나 이와같은 모델들은 낮은 차원의 파라메터를 통해서 3차원 사람을 표현하므로 옷을 입은 디테일을 표현의 모델을 위해서는 적합하지 않는다. 그랫거 템플릿 모델없이 티테일을 표현할수있는 방법을 제안한다. 그방법은...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/papers-0/",
         "teaser":null}]
